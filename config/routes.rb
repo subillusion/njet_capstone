e0dddf74758886fbdc3bb21a610f01850f9ac53e
Jesus Gonzalez <jg922965@gmail.com>
2019-06-07 16:02:07 +0000

    made changes to the list_controller_test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :lists
-  root 'lists#index'
-end
+   root 'lists#index'
+ end
--- test/controllers/lists_controller_test.rb
+++ test/controllers/lists_controller_test.rb
@@ -20,7 +20,7 @@ class ListsControllerTest < ActionDispatch::IntegrationTest
       post lists_url, params: { list: { completed: @list.completed, descripition: @list.descripition } }
     end
 
-    assert_redirected_to list_url(List.last)
+    assert_redirected_to root_url(List.last)
   end
 
   test "should show list" do
@@ -35,14 +35,15 @@ class ListsControllerTest < ActionDispatch::IntegrationTest
 
   test "should update list" do
     patch list_url(@list), params: { list: { completed: @list.completed, descripition: @list.descripition } }
-    assert_redirected_to list_url(@list)
+   # assert_redirected_to list_url(@list)
+   assert_redirected_to root_url
   end
 
   test "should destroy list" do
-    assert_difference('List.count', -1) do
+   assert_difference('List.count', -1) do
       delete list_url(@list)
     end
 
-    assert_redirected_to lists_url
+    assert_redirected_to root_url
   end
 end
