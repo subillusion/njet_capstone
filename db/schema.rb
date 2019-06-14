61455f8c17c2e04d35003d075781db5f9939a043
EC2 Default User <ec2-user@ip-172-31-21-255.us-west-2.compute.internal>
2019-06-14 16:25:29 +0000

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190607151458) do
+ActiveRecord::Schema.define(version: 20190610155826) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
-    t.string "descripition"
+    t.string "description"
     t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
