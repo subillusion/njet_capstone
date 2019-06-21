218ce822213026dfcf83c14472851f94e29b8b37
Timothy Danner <dannerman@hotmail.com>
2019-06-21 15:47:12 +0000

    Implement advanced login with sessions and remember token

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :lists
-  root 'lists#index'
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  root   'lists#index'
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   
   resources :users
 end
