0b991a591316cf1b21f61af54ed2680f8675dd4e
Timothy Danner <dannerman@hotmail.com>
2019-06-28 14:07:29 +0000

    Adding User Edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -18,6 +20,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
 private
 
   def user_params
@@ -25,4 +41,20 @@ private
                                  :password_confirmation)
   end
 
+  # Before filters
+  
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+  
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190610155826) do
+ActiveRecord::Schema.define(version: 20190621152251) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
